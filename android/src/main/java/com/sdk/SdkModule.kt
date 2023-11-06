4bf1dd1c3ee27650bdaea5b80f6dedc0ca58fd02
Matt Gacy <matt@mgacy.com>
2023-11-05 20:00:07 -0700

    Implement events

--- android/src/main/java/com/sdk/SdkModule.kt
+++ android/src/main/java/com/sdk/SdkModule.kt
@@ -5,6 +5,12 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.lytics.android.events.LyticsConsentEvent
+import com.lytics.android.events.LyticsEvent
+import com.lytics.android.events.LyticsIdentityEvent
+import com.lytics.android.logging.LogLevel
+import com.lytics.android.Lytics
+import com.lytics.android.LyticsConfiguration
 
 class SdkModule(reactContext: ReactApplicationContext) :
     ReactContextBaseJavaModule(reactContext) {
@@ -26,6 +32,68 @@ class SdkModule(reactContext: ReactApplicationContext) :
     @ReactMethod
     fun startWithApiToken(apiToken: String, options: ReadableMap) {
         // TODO: implement
+        val config = LyticsConfiguration(
+            apiKey = apiToken,
+        )
+    }
+
+    // Events
+
+    @ReactMethod
+    fun track(
+        stream: String? = null,
+        name: String? = null,
+        identifiers: ReadableMap? = null,
+        properties: ReadableMap? = null
+    ) {
+        Lytics.track(
+            LyticsEvent(
+                stream = stream,
+                name = name,
+                identifiers = identifiers?.toHashMap(),
+                properties = properties?.toHashMap()
+            )
+        )
+    }
+
+    @ReactMethod
+    fun identify(
+        stream: String? = null,
+        name: String? = null,
+        identifiers: ReadableMap? = null,
+        attributes: ReadableMap? = null,
+        shouldSend: Boolean = true
+    ) {
+        Lytics.identify(
+            LyticsIdentityEvent(
+                stream = stream,
+                name = name,
+                identifiers = identifiers?.toHashMap(),
+                attributes = attributes?.toHashMap(),
+                sendEvent = shouldSend
+            )
+        )
+    }
+
+    @ReactMethod
+    fun consent(
+        stream: String? = null,
+        name: String? = null,
+        identifiers: ReadableMap? = null,
+        attributes: ReadableMap? = null,
+        consent: ReadableMap? = null,
+        shouldSend: Boolean = true
+    ) {
+        Lytics.consent(
+            LyticsConsentEvent(
+                stream = stream,
+                name = name,
+                identifiers = identifiers?.toHashMap(),
+                attributes = attributes?.toHashMap(),
+                consent = consent?.toHashMap(),
+                sendEvent = shouldSend
+            )
+        )
     }
 
     companion object {
