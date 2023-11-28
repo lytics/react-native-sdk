c4681e879de931c72e93540efea4b57ba7a71deb
Matt Gacy <matt@mgacy.com>
2023-11-28 12:56:47 -0700

    Update example app namespace

--- example/android/app/src/release/java/com/lytics/react_native/example/ReactNativeFlipper.java
+++ example/android/app/src/release/java/com/lytics/react_native/example/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.sdkexample;
+package com.lytics.react_native.example;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
