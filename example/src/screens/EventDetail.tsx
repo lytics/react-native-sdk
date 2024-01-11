333a411a8aa409424695f786a1a5e33144f982f6
Matt Gacy <matt@mgacy.com>
2024-01-11 14:35:45 -0700

    Use SDK in Event Detail

--- example/src/screens/EventDetail.tsx
+++ example/src/screens/EventDetail.tsx
@@ -1,11 +1,24 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
+import { screen, track } from 'react-native-sdk';
 
 import { styles } from '../components/Styles';
 
 export function EventDetailScreen() {
+  useEffect(() => {
+    console.log('Load Event Detail');
+    screen({
+      name: 'Event_Detail',
+      properties: { artistID: 123, eventID: 345 },
+    });
+  }, []);
+
   const handleBuy = () => {
     console.log('Buy');
+    track({
+      name: 'Buy_Tickets',
+      properties: { artistID: 123, eventID: 345 },
+    });
   };
 
   return (
