a331b2330a74f6473e7bb5076a81eb023e4693d8
Matt Gacy <matt@mgacy.com>
2024-01-15 12:25:39 -0700

    Use SDK in Profile

--- example/src/screens/Profile.tsx
+++ example/src/screens/Profile.tsx
@@ -1,10 +1,25 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, ScrollView } from 'react-native';
+import { getProfile } from 'react-native-sdk';
 
 import { styles } from '../components/Styles';
 
 export function ProfileScreen() {
-  const [data, setData] = React.useState({});
+  const [data, setData] = useState({});
+
+  const loadProfile = async () => {
+    try {
+      const profile = await getProfile();
+      console.log('Profile:', profile);
+      setData(profile);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -13,7 +28,7 @@ export function ProfileScreen() {
         format:
       </Text>
       <ScrollView style={styles.scrollView}>
-        <Text>{JSON.stringify(data)}</Text>
+        <Text>{JSON.stringify(data, null, 2)}</Text>
       </ScrollView>
     </View>
   );
